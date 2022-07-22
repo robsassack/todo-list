74d27a86cfd686cedaadec5299612ebb95258864
Rob Sassack <rsassack25@gmail.com>
2022-07-22 07:13:18 -0400

    Added placeholder form to create new todo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import Overview from "./Overview";
 import Project from "./Project";
 import Todo from "./Todo";
+
+document.querySelector("#submit").addEventListener("click", () => {
+  let title = document.querySelector("#title").value;
+  let desc = document.querySelector("#desc").value;
+  let date = Date(document.querySelector("#date"));
+  let priority = document.querySelector("#priority").value;
+  let project = document.querySelector("#project").value;
+  let newTodo = new Todo(title, desc, date, priority, project);
+  console.log(newTodo);
+});
