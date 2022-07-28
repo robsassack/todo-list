0e135289a563d2fe7e62958d387016b39bd091bc
Rob Sassack <rsassack25@gmail.com>
2022-07-28 01:14:11 -0400

    Added some minor CSS to todo list

--- src/Overview.js
+++ src/Overview.js
@@ -1,10 +1,11 @@
-import Tag from './Tag';
+import Tag from "./Tag";
 
 export default class Overview {
   constructor() {
     this.tags = [];
   }
 
+  // adds tag to list
   addTag(tag) {
     if (this.tags.length === 0 || this.uniqueTag(tag)) {
       // creates new tag if no tags exist or if tag is unique
@@ -13,9 +14,10 @@ export default class Overview {
     }
   }
 
+  // checks if tag is unique
   uniqueTag(tag) {
     let repeat = true;
-    this.tags.forEach(item => {
+    this.tags.forEach((item) => {
       if (item.name === tag) {
         // sets flag if repeat tag is found
         repeat = false;
@@ -24,21 +26,24 @@ export default class Overview {
     return repeat;
   }
 
+  // adds todo to tag
   addTodo(title, desc, date, priority, tag) {
     this.addTag(tag);
-    this.tags.forEach(item => {
+    this.tags.forEach((item) => {
       if (item.name === tag) {
         item.addTodo(title, desc, date, priority, tag);
       }
     });
   }
 
+  // prints all todos
   printAllTodos() {
     let todoList = document.querySelector("#todo-list");
     todoList.innerText = "";
-    this.tags.forEach(tag => {
-      tag.getTodoList().forEach(todo => {
+    this.tags.forEach((tag) => {
+      tag.getTodoList().forEach((todo) => {
         let newTodo = document.createElement("li");
+        newTodo.classList.add("todo-item");
         newTodo.classList.add(setPriority(todo.priority));
         newTodo.innerText = `[${todo.priority}] ${todo.title} - ${todo.desc} - ${todo.dueDate} - ${todo.tag}`;
         todoList.appendChild(newTodo);
@@ -47,6 +52,7 @@ export default class Overview {
   }
 }
 
+// sets priority class
 function setPriority(priority) {
   switch (priority) {
     case "high":
