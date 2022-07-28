d4171f86eea2927da128311e4460b45b630b7767
Rob Sassack <rsassack25@gmail.com>
2022-07-28 15:02:28 -0400

    Added functionality to delete todos

--- src/Overview.js
+++ src/Overview.js
@@ -42,13 +42,36 @@ export default class Overview {
     todoList.innerText = "";
     this.tags.forEach((tag) => {
       tag.getTodoList().forEach((todo) => {
+        // todo item
         let newTodo = document.createElement("li");
         newTodo.classList.add("todo-item");
         newTodo.classList.add(setPriority(todo.priority));
         newTodo.innerText = `[${todo.priority}] ${todo.title} - ${todo.desc} - ${todo.dueDate} - ${todo.tag}`;
+
+        // delete button
+        let delButton = document.createElement("button");
+        delButton.classList.add("del-button");
+        delButton.innerText = "X";
+        delButton.addEventListener("click", () => {
+          this.deleteTodo(todo);
+        });
+
+        newTodo.appendChild(delButton);
         todoList.appendChild(newTodo);
       });
     });
+  }
+
+  // deletes todo
+  deleteTodo(todo) {
+    this.tags.forEach((tag) => {
+      tag.getTodoList().forEach((item) => {
+        if (item === todo) {
+          tag.list.splice(tag.list.indexOf(item), 1);
+        }
+      });
+    });
+    this.printAllTodos();
   }
 }
 
