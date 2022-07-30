3d3684c37554fefb5f0a76eb7d14075677da8be7
Rob Sassack <rsassack25@gmail.com>
2022-07-30 01:57:41 -0400

    Changed todo creation menu to open with a button

--- src/main.js
+++ src/main.js
@@ -25,6 +25,16 @@ document.querySelector("#todo-submit").addEventListener("click", () => {
   mainOverview.addTodo(title, desc, date, priority, tag);
 
   mainOverview.printAllTodos();
+
+  let todoForm = document.querySelector(".todo-form");
+  let formContainer = document.querySelector(".form-container");
+  todoForm.style.visibility = "hidden";
+  formContainer.style.visibility = "hidden";
+  document.querySelector("#title").value = "";
+  document.querySelector("#desc").value = "";
+  document.querySelector("#dueDate").value = "";
+  document.querySelector("#priority").value = "low";
+  document.querySelector("#tag").value = "";
 });
 
 document.querySelector("#selector").addEventListener("change", () => {
@@ -36,6 +46,24 @@ document.querySelector("#selector").addEventListener("change", () => {
   } else if (selector.startsWith("tag:")) {
     let tag = selector.split(":")[1];
     mainOverview.printTagTodos(tag);
+  }
+});
+
+document.querySelector("#add-todo-menu").addEventListener("click", () => {
+  let todoForm = document.querySelector(".todo-form");
+  let formContainer = document.querySelector(".form-container");
+  todoForm.style.visibility = "visible";
+  formContainer.style.visibility = "visible";
+});
+
+document.querySelector(".form-container").addEventListener("click", function(e) {
+  if (document.querySelector(".todo-form").contains(e.target)) {
+    return;
+  } else {
+    let todoForm = document.querySelector(".todo-form");
+    let formContainer = document.querySelector(".form-container");
+    todoForm.style.visibility = "hidden";
+    formContainer.style.visibility = "hidden";
   }
 });
 
